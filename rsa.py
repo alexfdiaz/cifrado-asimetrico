b6ca0a042541ce8351dfd444fc5cd7aa584e3fea
Alex Diaz <alexdiaz18012002@gmail.com>
2021-05-09 14:48:50 -0300

    Documentacion de rsa.py

--- rsa.py
+++ rsa.py
@@ -1,5 +1,8 @@
 from random import *
 
+#Funciones para la generacion del par de claves (desde linea 3 hasta linea 50)
+
+#Funcion que toma un numero y devuelve un valor booleano en relacion a si es primo o no
 def isPrime(num):
 	divisores = 0
 	for i in range(num):
@@ -8,6 +11,7 @@ def isPrime(num):
 	if divisores < 3: return True
 	else: return False
 
+#Generador aleatorio de numeros primos. Primero genera un numero cualquiera y despues comprueba si es primo, si lo es devuelve el numero
 def randomPrime(num):
 	seed(getstate())
 	ran = randint(num, 2 * num)
@@ -17,6 +21,7 @@ def randomPrime(num):
 			ran = num
 	return ran
 
+#Funcion que calcula el minimo comun divisor (para luego comprobar si dos numeros son coprimos)
 def mcd(a, b):
 	resto = 0
 	while(b > 0):
@@ -25,10 +30,12 @@ def mcd(a, b):
 		a = resto
 	return a
 
+#Funcion que toma dos numeros y devuelve un booleano en relacion a si son coprimos o no
 def areCoprime(a, b):
 	if mcd(a, b) == 1: return True
 	else: return False
 
+#Funcion que toma un numero  n y genera un coprimo aleatorio en el rango (1, n-1)
 def randomCoprime(num):
 	while True:
 		seed(getstate())
@@ -36,16 +43,13 @@ def randomCoprime(num):
 		if areCoprime(ran, num):
 			return ran
 
+#Funcion que genera un exponente de la clave privada
 def randomD(e, fi):
-	'''while True:
-		seed(getstate())
-		ran = randint(1, fi)
-		if (e * ran - 1) % fi == 0:
-			return ran'''
 	for d in range(2, fi):
 		if (e * d - 1) % fi == 0:
 			return d
 
+#Funcion que genera un numero aleatorio que cumpla que al dividirlo por div tenga el mismo resto que mod
 def findByMod(num, mod, div):
 	while True:
 		seed(getstate())
@@ -53,6 +57,7 @@ def findByMod(num, mod, div):
 		if ran % div == mod:
 			return ran
 
+#Funcion que encripta el mensaje introducido
 def cifrar(msje, publicKey):
 	lista = []
 	msjeCifrado = []
@@ -64,6 +69,8 @@ def cifrar(msje, publicKey):
 		msjeCifrado.append(findByMod(msjeExp, mod, publicKey[0]))
 	return msjeCifrado
 
+
+#Funcion que desencripta el mensaje introducido
 def descifrar(msjeCifrado, privateKey):
 	msjeDescifrado = []
 	for i in msjeCifrado:
